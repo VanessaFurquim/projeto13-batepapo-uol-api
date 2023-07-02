08826e64a145a953eb13a098550bea5ba6053143
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-07-01 23:58:19 -0300

    POST route /messages: validation message format done, untested; validation of participant logged in to chat room  done, untested; message saved to database done, untested

--- src/app.js
+++ src/app.js
@@ -38,14 +38,6 @@ app.post("/participants", async (req, res) => {
     return res.sendStatus(422);
   }
 
-  const validationMessages = schemaMessages.validate(req.body, {abortEarly: false});
-
-  if (validationMessages.error) {
-    const errors = validationMessages.error.details.map((detail) => detail.message); /* tratamento da validação messages */
-    console.log(errors);
-    return res.sendStatus(422);
-  }
-
   try {
     const participant = await db.collection("participants").findOne({ name: name }); /* condição de participante existente */
     if (participant) return res.sendStatus(409);
@@ -74,6 +66,42 @@ app.get("participants", async (req, res) => {
 		res.send(activeParticipants);
 
 	} catch (error) {res.status(500).send(error.message)}
+});
+
+app.post("messages", async (req, res) => {
+	const { to, text, type } = req.body;
+	const { User } = req.headers;
+
+	const schemaMessages = joi.object({
+		to: joi.string().required(),
+		text: joi.string().required(),
+		type: joi.string().valid("message", "private_message").required()
+	});
+
+	const validationMessages = schemaMessages.validate(req.body, {abortEarly: false});
+
+	if (validationMessages.error) {
+		const errors = validationMessages.error.details.map((detail) => detail.message);
+		console.log(errors);
+		return res.sendStatus(422);
+	};
+
+	try {
+		const activeParticipant = await db.collection("participants").findOne({ User }).toArray();
+
+		if (!activeParticipant) {return res.sendStatus(404)};
+
+		await db.collection("messages").insertOne({
+			from: User,
+			to: to,
+			text: text,
+			type: type,
+			time: dayjs().format("HH:mm:ss")
+		  });
+
+		res.sendStatus(201);
+
+	} catch (error) {res.status(500).send(error.message)};
 });
 
 const PORT = 5000;
