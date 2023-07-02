dc231b3f3dcf334713f4449b9d83dfacccb1084d
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-07-02 01:34:07 -0300

    GET route /messages: finding messages that user can see using  done, untested; conditions with the limit stablisehd by user done (with function switch) done, untested;

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ app.get("participants", async (req, res) => {
 		if (activeParticipants === undefined) {return res.status(404).send("[]")};
 		res.send(activeParticipants);
 
-	} catch (error) {res.status(500).send(error.message)}
+	} catch (error) {res.status(500).send(error.message)};
 });
 
 app.post("messages", async (req, res) => {
@@ -102,6 +102,28 @@ app.post("messages", async (req, res) => {
 		res.sendStatus(201);
 
 	} catch (error) {res.status(500).send(error.message)};
+});
+
+app.get("messages", async (req, res) => {
+	const { User } = req.headers;
+	const limit = parseInt(req.query.limit);
+
+	try {
+		const messagesForUser = await db.messages.find( { $or: [ { type: "message" }, { to: User }, {from: User } ] } );
+
+		switch (limit) {
+			case !limit:
+				res.send(messagesForUser);
+				break;
+			case limit > 0:
+				const limitedMessages = messagesForUser.slice((limit));
+				res.send(limitedMessages);
+				break;
+			case limit <= 0:
+				res.sendStatus(422);
+				break;
+		};
+	}  catch (error) {res.status(500).send(error.message)};
 });
 
 const PORT = 5000;
